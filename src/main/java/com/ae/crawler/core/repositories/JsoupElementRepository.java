0ea9eb196b8f46bd11b4b6a68189cd0db6fedaf8
Diego Perez <diego.perez@etermax.com>
2020-11-12 20:17:50 -0300

    Create Element repository

---
+++ src/main/java/com/ae/crawler/core/repositories/JsoupElementRepository.java
@@ -0,0 +1,23 @@
+package com.ae.crawler.core.repositories;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+public class JsoupElementRepository {
+
+	private static final String CHARSET_NAME = "utf8";
+	private final Document document;
+
+	public JsoupElementRepository(File resource) throws IOException {
+		document = Jsoup.parse(resource, CHARSET_NAME, resource.getAbsolutePath());
+	}
+
+	public Optional<Element> findById(String elementId) {
+		return Optional.ofNullable(document.getElementById(elementId));
+	}
+}
--- src/test/java/com/ae/crawler/core/actions/LookupTest.java
+++ src/test/java/com/ae/crawler/core/actions/LookupTest.java
@@ -1,12 +1,31 @@
 package com.ae.crawler.core.actions;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+
+import org.jsoup.nodes.Element;
 import org.junit.Test;
+
+import com.ae.crawler.core.repositories.JsoupElementRepository;
 
 public class LookupTest {
 
 	@Test
-	public void testElementIsFoundInDocument() {
+	public void testElementIsFoundInDocument() throws IOException {
+		// given
+		String elementId = "make-everything-ok-button";
+		String resourcePath = "./src/test/resources/sample-0-origin.html";
 
+		JsoupElementRepository repository = new JsoupElementRepository(new File(resourcePath));
+
+		// when
+		Optional<Element> element = repository.findById(elementId);
+
+		// then
+		assertThat(element).isNotEmpty();
 	}
 
 	@Test
