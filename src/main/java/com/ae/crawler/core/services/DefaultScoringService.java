5a2193f4a2ec47a31acf50895ddbd7b35e4c8286
Diego Perez <diego.perez@etermax.com>
2020-11-20 22:15:20 -0300

    Refactor

--- src/main/java/com/ae/crawler/core/services/DefaultScoringService.java
+++ src/main/java/com/ae/crawler/core/services/DefaultScoringService.java
@@ -1,20 +1,13 @@
 package com.ae.crawler.core.services;
 
-import java.util.Map;
-
 import org.jsoup.nodes.Element;
 
 public class DefaultScoringService implements ScoringService {
 	@Override
 	public Integer getScore(Element element, Element similarElement) {
-		Integer score = 0;
-
-		for (Map.Entry<String, String> entry : similarElement.attributes()) {
-			if (element.attributes().hasKey(entry.getKey()) &&
-					element.attributes().get(entry.getKey()).equals(entry.getValue())) {
-				score++;
-			}
-		}
-		return score;
+		return Math.toIntExact(similarElement.attributes().asList().stream().filter(attribute ->
+			element.attributes().hasKey(attribute.getKey()) &&
+					element.attributes().get(attribute.getKey()).equals(attribute.getValue())
+		).count());
 	}
 }
